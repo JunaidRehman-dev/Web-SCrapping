90631e483e239d1c14ba7a3a674d5e5b82709cd0
Junaid Rehman <junaidarain18465@gmail.com>
2025-07-06 22:17:07 +0500

    Update zameen.com.py

--- zameen.com.py
+++ zameen.com.py
@@ -1,11 +1,11 @@
-import requests
+import requests                                         #loading essential libraries and packages
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import random
 
 user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",                          #using multilpe user agents to avoid  bots set by website
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
     "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0)",
     "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X)...",
@@ -17,23 +17,23 @@ user_agents = [
 
 
 Master_list = {
-    'Price 💲': [],'Location 🗺️': [],'Beds 🛏️': [],'Baths 🛁': [],'Area 📏': [],
-'Image 🚠': [],'For more Details ❓': [],'Published Time⏲️': []
+    'Price 💲': [],'Location 🗺️': [],'Beds 🛏️': [],'Baths 🛁': [],'Area 📏': [],        #creating a master list to add all the lists 
+'Image 🚠': [],'For more Details ❓': [],'Published Time⏲️': []                          to a final main list
 }
 
 for i in range(1,5):
-    url = f"https://www.zameen.com/Homes/Lahore_DHA_Defence-9-{i}.html"
+    url = f"https://www.zameen.com/Homes/Lahore_DHA_Defence-9-{i}.html"                   #scrapping through 5 pages of the desired website
     
 
     headers = {
-            "User-Agent": random.choice(user_agents)}
-    print(f"Scraping: {url}")
+            "User-Agent": random.choice(user_agents)}                                     #calling the headers and usind random package to randomly 
+    print(f"Scraping: {url}")                                                              use the headers
 
 
 
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
-    time.sleep(random.uniform(3, 7))
+    time.sleep(random.uniform(3, 7))                                                   # adding time-delay in our requests for safety
 
 
 
@@ -81,11 +81,11 @@ for i in range(1,5):
         
     
     min_len = min(len(col) for col in data.values())
-    for key in data:
+    for key in data:                                                               #adjusting the elements to a same number to avoid errrors
         data[key] = data[key][:min_len]
-        Master_list[key].extend(data[key])   
+        Master_list[key].extend(data[key])                                         #combining all the lists to a  master list 
 
 df=pd.DataFrame.from_dict(Master_list)
-df.to_excel("zameen_listings_all_pages.xlsx", index=False)
-print("✅ Scraping complete. Data saved to 'zameen_listings_all_pages.xlsx'")
+df.to_excel("zameen_listings_all_pages.xlsx", index=False)                      storing the master list in a newly created ecxel sheet 
+print("✅ Scraping complete. Data saved to 'zameen_listings_all_pages.xlsx'")     
 
